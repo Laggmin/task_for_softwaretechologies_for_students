0d2d30e45e7261847b9585df6054e05325416c8e
aksedov <Laggmin05@gmail.com>
2024-12-13 09:29:52 +0300

    ready

--- src/main/java/org/softwaretechnologies/EmployeeFactory.java
+++ src/main/java/org/softwaretechnologies/EmployeeFactory.java
@@ -7,17 +7,20 @@ public class EmployeeFactory {
     /**
      * Сначала создайте классы, наследуемые от  {@link  org.softwaretechnologies.employee.Employee Employee} для каждого из значений в EmployeeType.
      * Функция должна создавать и возвращать Employee нужного типа. Тип зависит от значения параметра type.
-     *  name имя сотрудника
-     *  baseSalary базовая зарплата сотрудника
-     *  type тип сотрудника
+     * name имя сотрудника
+     * baseSalary базовая зарплата сотрудника
+     * type тип сотрудника
+     *
      * @return созданного сотрудника нужного типа. Тип зависит от параметра type.
      */
 
 
-
     public static Employee createEmployee(String name, int baseSalary, EmployeeType type) {
         // TODO: реализуйте вышеуказанную функцию
-
-        return null;
+        return switch (type) {
+            case Tester -> new Tester(name, baseSalary);
+            case Manager -> new Manager(name, baseSalary);
+            case Programmer -> new Programmer(name, baseSalary);
+        };
     }
 }
